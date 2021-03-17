297b5f8d93ca57f6f765e2184f6ba1eaee0fc969
williamesanders05 <williamesanders05@gmail.com>
2021-03-17 00:41:46 -0500

    added user and set models plus added login, logout, register, and index functions

--- flashcard/models.py
+++ flashcard/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class User(AbstractUser):
+    pass
+
+class Set(models.Model):
+    title = models.CharField(max_length = 50)
+    owner = models.CharField(max_length = 20, null=True)
+    term = models.CharField(max_length = 50)
+    description = models.CharField(max_length = 100)
+    image = models.URLField(max_length = 1000)
--- testlet/settings.py
+++ testlet/settings.py
@@ -81,7 +81,7 @@ DATABASES = {
     }
 }
 
-
+AUTH_USER_MODEL = 'flashcard.User'
 # Password validation
 # https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
 
