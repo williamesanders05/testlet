9555c333796cf2d1deec95effdfbbf76f4f442df
williamesanders05 <williamesanders05@gmail.com>
2021-05-20 14:18:49 -0500

    fixed index page and models

--- flashcard/models.py
+++ flashcard/models.py
@@ -5,9 +5,13 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     pass
 
+class Terms(models.Model):
+    term = models.CharField(max_length = 100)
+    definition = models.CharField(max_length = 100)
+    image = models.URLField(max_length = 10000, null = True, blank = True)
+    set = models.IntegerField(default=None, null=True)
+
 class Set(models.Model):
     title = models.CharField(max_length = 50)
     owner = models.CharField(max_length = 20, null=True)
-    term = models.CharField(max_length = 50)
     description = models.CharField(max_length = 100)
-    image = models.URLField(max_length = 1000)
